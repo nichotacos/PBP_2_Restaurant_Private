5974dc9b5fd2e7fdcf4d1c08642c767eae42914a
KevinJr88 <99953819+KevinJr88@users.noreply.github.com>
2023-09-24 20:50:55 +0700

    Dark mode & Light Mode

--- lib/login.dart
+++ lib/login.dart
@@ -10,6 +10,20 @@ class LoginView extends StatefulWidget {
   State<LoginView> createState() => _LoginViewState();
 }
 
+bool _iconBool = false;
+
+IconData _iconLight = Icons.wb_sunny;
+IconData _iconDark = Icons.nights_stay;
+
+ThemeData _lightTheme = ThemeData(
+  primarySwatch: Colors.amber,
+  brightness: Brightness.light,
+);
+ThemeData _darkTheme = ThemeData(
+  primarySwatch: Colors.red,
+  brightness: Brightness.dark,
+);
+
 class _LoginViewState extends State<LoginView> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController usernameController = TextEditingController();
@@ -19,128 +33,143 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     Map? dataForm = widget.data;
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: Center(
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextFormField(
-                        controller: usernameController,
-                        validator: (p0) {
-                          if (p0 == null || p0.isEmpty) {
-                            return "username tidak boleh kosong";
-                          }
-                          return null;
-                        },
-                        decoration: const InputDecoration(
-                          hintText: "Username",
-                          labelText: "Inputkan User yang telah didaftar",
-                          icon: Icon(Icons.person),
-                        ),
-                      ),
-                      TextFormField(
-                        controller: passwordController,
-                        obscureText: !showPassword,
-                        validator: (p0) {
-                          if (p0 == null || p0.isEmpty) {
-                            return "password kosong";
-                          }
-                          return null;
-                        },
-                        decoration: InputDecoration(
-                          hintText: "Password",
-                          labelText: "Inputkan Password",
-                          icon: Icon(Icons.password),
-                          suffixIcon: IconButton(
-                            icon: Icon(showPassword
-                                ? Icons.visibility
-                                : Icons.visibility_off),
-                            onPressed: () {
-                              setState(() {
-                                showPassword = !showPassword;
-                              });
-                            },
+    return MaterialApp(
+      theme: _iconBool ? _darkTheme : _lightTheme,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: usernameController,
+                          validator: (p0) {
+                            if (p0 == null || p0.isEmpty) {
+                              return "username tidak boleh kosong";
+                            }
+                            return null;
+                          },
+                          decoration: const InputDecoration(
+                            hintText: "Username",
+                            labelText: "Inputkan User yang telah didaftar",
+                            icon: Icon(Icons.person),
                           ),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              if (_formKey.currentState!.validate()) {
-                                if (dataForm!['username'] ==
-                                        usernameController.text &&
-                                    dataForm['password'] ==
-                                        passwordController.text) {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => const HomeView(),
-                                    ),
-                                  );
-                                } else {
-                                  showDialog(
-                                    context: context,
-                                    builder: (_) => AlertDialog(
-                                      title: const Text('Password Salah'),
-                                      content: TextButton(
-                                        onPressed: () => pushRegister(context),
-                                        child: const Text('Daftar Disini !!'),
+                        TextFormField(
+                          controller: passwordController,
+                          obscureText: !showPassword,
+                          validator: (p0) {
+                            if (p0 == null || p0.isEmpty) {
+                              return "password kosong";
+                            }
+                            return null;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            labelText: "Inputkan Password",
+                            icon: Icon(Icons.password),
+                            suffixIcon: IconButton(
+                              icon: Icon(showPassword
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                              onPressed: () {
+                                setState(() {
+                                  showPassword = !showPassword;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  if (dataForm!['username'] ==
+                                          usernameController.text &&
+                                      dataForm['password'] ==
+                                          passwordController.text) {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => const HomeView(),
                                       ),
-                                      actions: <Widget>[
-                                        TextButton(
+                                    );
+                                  } else {
+                                    showDialog(
+                                      context: context,
+                                      builder: (_) => AlertDialog(
+                                        title: const Text('Password Salah'),
+                                        content: TextButton(
                                           onPressed: () =>
-                                              Navigator.pop(context),
-                                          child: const Text('Cancel'),
+                                              pushRegister(context),
+                                          child: const Text('Daftar Disini !!'),
                                         ),
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                            pushRegister(context);
-                                          },
-                                          child: const Text('OK'),
-                                        ),
-                                      ],
-                                    ),
-                                  );
+                                        actions: <Widget>[
+                                          TextButton(
+                                            onPressed: () =>
+                                                Navigator.pop(context),
+                                            child: const Text('Cancel'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                              pushRegister(context);
+                                            },
+                                            child: const Text('OK'),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }
                                 }
-                              }
-                            },
-                            child: const Text('Login'),
-                          ),
-                        ],
-                      ),
-                    ],
+                              },
+                              child: const Text('Login'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const Divider(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    pushLogin(context);
-                  },
-                  child: const Text('Login'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    pushRegister(context);
-                  },
-                  child: const Text('Register'),
-                ),
-              ],
-            ),
-          ],
+              const Divider(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      pushLogin(context);
+                    },
+                    child: const Text('Login'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      pushRegister(context);
+                    },
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Tindakan yang ingin Anda lakukan saat FAB ditekan
+            setState(() {
+              _iconBool = !_iconBool;
+            });
+          }, // Icon yang akan ditampilkan di FAB
+          backgroundColor: Colors.blue,
+          child: Icon(
+              _iconBool ? _iconDark : _iconLight), // Warna latar belakang FAB
         ),
       ),
     );
