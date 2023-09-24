73affd86b0727e259c87d22481e8e656477a698d
Minoritydsp <davesebastian17@gmail.com>
2023-09-24 14:23:43 +0700

    update sign up dart

--- lib/view/signUp.dart
+++ lib/view/signUp.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pbp_2_restaurant/View/signUp.dart';
-import 'package:pbp_2_restaurant/View/form_component.dart';
+import 'package:pbp_2_restaurant/View/login.dart';
+import 'package:pbp_2_restaurant/component/form_component.dart';
 
-class signUpView extends statefulWidget {
+class signUpView extends StatefulWidget {
   const signUpView({super.key});
 
   @override
@@ -15,5 +15,118 @@ class _signUpViewState extends State<signUpView> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController notelpController = TextEditingController();
-  TextEditingController date = TextEditingController();
+  TextEditingController birthDateController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              inputForm((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Username Tidak Boleh Kosong';
+                }
+                if (p0.toLowerCase() == 'anjing') {
+                  return 'Tidak Boleh Menggunakan kata kasar';
+                }
+                return null;
+              },
+                  controller: usernameController,
+                  hintTxt: "Username",
+                  helperTxt: "Ucup Surucup",
+                  iconData: Icons.person),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Email tidak boleh kosong';
+                }
+                if (!p0.contains('@')) {
+                  return 'Email harus menggunakan @';
+                }
+                return null;
+              }),
+                  controller: emailController,
+                  hintTxt: "Email",
+                  helperTxt: "ucup@gmail.com",
+                  iconData: Icons.email),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Password tidak boleh kosong';
+                }
+                if (p0.length < 5) {
+                  return 'Password minimal 5 digit';
+                }
+                return null;
+              }),
+                  controller: passwordController,
+                  hintTxt: "Password",
+                  helperTxt: "xxxxxxx",
+                  iconData: Icons.password,
+                  password: true),
+              inputForm(((p0) {
+                if (p0 == null || p0.isEmpty) {
+                  return 'Nomor telepon tidak boleh kosong';
+                }
+                return null;
+              }),
+                  controller: notelpController,
+                  hintTxt: "No Telp",
+                  helperTxt: "082123456789",
+                  iconData: Icons.phone_android),
+              TextFormField(
+                controller: birthDateController,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  hintText: 'Tanggal Lahir',
+                  prefixIcon: Icon(Icons.calendar_today),
+                  labelText: 'Tanggal Lahir',
+                  helperText: 'Pilih Tanggal Lahir Anda',
+                ),
+                onTap: () {
+                  showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1960),
+                    lastDate: DateTime.now(),
+                  ).then((pickedDate) {
+                    if (pickedDate != null) {
+                      setState(() {
+                        birthDateController.text =
+                            pickedDate.toLocal().toString().split(' ')[0];
+                      });
+                    }
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Tanggal Lahir tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      Map<String, dynamic> formData = {};
+                      formData['username'] = usernameController.text;
+                      formData['password'] = passwordController.text;
+
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (_) => LoginView(
+                                    data: formData,
+                                  )));
+                    }
+                  },
+                  child: const Text('Register'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
